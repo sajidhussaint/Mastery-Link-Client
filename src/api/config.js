1c62ad8fd674f3e799a7ced0cef5941d63e0ec6c
sajid <sajidtmpm@gmail.com>
2024-05-08 20:17:08 +0530

    feat:Added Koyeb Hosting,Base Url

--- src/api/config.js
+++ src/api/config.js
@@ -1,33 +1,32 @@
-import axios from "axios"
+import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/"
+// const BASE_URL = "http://localhost:3000/"
+const BASE_URL = "https://psychological-jori-masterylink-e6778f0d.koyeb.app/";
 
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL
-})
+  baseURL: BASE_URL,
+});
 
 export const axiosAuthorized = axios.create({
-  baseURL: BASE_URL
-})
+  baseURL: BASE_URL,
+});
 
 axiosAuthorized.interceptors.request.use(
-  config => {
-    const token = localStorage.getItem("token")
+  (config) => {
+    const token = localStorage.getItem("token");
     if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
-    return config
+    return config;
   },
-  error => {
-    return Promise.reject(error)
+  (error) => {
+    return Promise.reject(error);
   }
-)
+);
 
 axiosAuthorized.interceptors.response.use(
-  response => response,
-  error => {
-    return Promise.reject(error)
+  (response) => response,
+  (error) => {
+    return Promise.reject(error);
   }
-)
-
-
+);
