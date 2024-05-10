cd80a550dcac67a4bcb206b35a8336bf9c3f55ce
sajid <sajidtmpm@gmail.com>
2024-05-10 11:30:33 +0530

    fix:changed origin socket io

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "http://localhost:3000/"
-const BASE_URL = "https://psychological-jori-masterylink-e6778f0d.koyeb.app/";
+const BASE_URL = "http://localhost:3000/"
+// const BASE_URL = "https://psychological-jori-masterylink-e6778f0d.koyeb.app/";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
